12fe93478a7b65d60c0b06ff2a97e5440fc983ff
ваше имя <dimarik-tixonov@yandex.ru>
2022-12-04 22:27:47 +0300

    fix: поправит ссылку на репозиторий

--- src/layout/header/header.tsx
+++ src/layout/header/header.tsx
@@ -7,9 +7,17 @@ export function Header(): JSX.Element {
     <header>
       <nav className={cn(styles.nav, 'blue-grey darken-2')}>
         <div className="nav-wrapper">
-          <a href="!#" className="brand-logo">Logo</a>
+          <a href="/#" className="brand-logo">React Movies</a>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><a href="sass.html">Repo</a></li>
+            <li>
+              <a
+                href="https://github.com/limestormrage/video-list"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Repo
+              </a>
+            </li>
           </ul>
         </div>
       </nav>
