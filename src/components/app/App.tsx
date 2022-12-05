e250d218924009effd7b4ad8285b03146e40e11f
ваше имя <dimarik-tixonov@yandex.ru>
2022-12-05 21:28:42 +0300

    add: Добавит компонент мэйн

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Footer } from '../../layout/footer/footer';
 import { Header } from '../../layout/header/header';
+import { Main } from '../../layout/main/main';
 import './App.css';
 
 function App(): JSX.Element {
   return (
     <div className="App">
       <Header />
+      <Main />
       <Footer />
     </div>
   );
---
+++ src/layout/main/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Main(): JSX.Element {
+  return (
+    <main className="main">
+      s
+    </main>
+  );
+}
