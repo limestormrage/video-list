cd7eaba53cd92d265d38d78ad93178ba59aeeee9
ваше имя <dimarik-tixonov@yandex.ru>
2022-12-14 19:23:56 +0300

    add: добавит фильтрацию

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,15 +1,27 @@
-import React, { KeyboardEvent, useState } from 'react';
+import React, { ChangeEvent, KeyboardEvent, useState } from 'react';
 import cn from 'classnames';
 import { ISearchProps } from './search.props';
 import styles from './search.module.scss';
 
+const initialState = {
+  search: '',
+  type: 'all',
+};
+
 export function Search({ SearchCallBack }: ISearchProps): JSX.Element {
-  const [search, setSearch] = useState<string>('');
+  const [formState, setFormState] = useState(initialState);
 
   const handleKye = (e: KeyboardEvent<HTMLInputElement>): void => {
     if (e.key === 'Enter') {
-      SearchCallBack(search);
+      SearchCallBack(formState.search);
     }
+  };
+
+  const onChangeFilter = ({ target }: ChangeEvent<HTMLInputElement>): void => {
+    setFormState((prevState) => ({
+      ...prevState,
+      type: target.value,
+    }));
   };
 
   return (
@@ -19,17 +31,55 @@ export function Search({ SearchCallBack }: ISearchProps): JSX.Element {
         id="password"
         type="search"
         className="validate"
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
+        value={formState.search}
+        onChange={(e) => setFormState((prevState) => ({
+          ...prevState,
+          search: e.target.value,
+        }))}
         onKeyDown={handleKye}
       />
       <button
         className={cn(styles.searchButton, 'btn')}
         type="button"
-        onClick={() => SearchCallBack(search)}
+        onClick={() => SearchCallBack(formState.search)}
       >
         Search
       </button>
+      <div>
+        <label className={styles.filterLabel}>
+          <input
+            className="with-gap"
+            name="type"
+            type="radio"
+            value="all"
+            onChange={onChangeFilter}
+            checked={formState.type === 'all'}
+          />
+          <span>All</span>
+        </label>
+        <label className={styles.filterLabel}>
+          <input
+            className="with-gap"
+            name="type"
+            type="radio"
+            value="movie"
+            onChange={onChangeFilter}
+            checked={formState.type === 'movie'}
+          />
+          <span>Movies only</span>
+        </label>
+        <label className={styles.filterLabel}>
+          <input
+            className="with-gap"
+            name="type"
+            type="radio"
+            value="series"
+            onChange={onChangeFilter}
+            checked={formState.type === 'series'}
+          />
+          <span>Series only</span>
+        </label>
+      </div>
     </div>
   );
 }
